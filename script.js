434af88fc542bf78c5669f88b75e926e2513bfae
Me <andrianeko.037@gmail.com>
2019-11-25 09:14:03 +0700

    Link

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var o = $(".card");
-$("#top").on("mousemove", function (t) {
-    var e = -($(window).innerWidth() / 2 - t.pageX) / 30,
-        n = ($(window).innerHeight() / 2 - t.pageY) / 10;
-    o.attr("style", "transform: rotateY(" + e + "deg) rotateX(" + n + "deg);-webkit-transform: rotateY(" + e + "deg) rotateX(" + n + "deg);-moz-transform: rotateY(" + e + "deg) rotateX(" + n + "deg)")
-})
-
-function Exp(){
-        window.location='http://bit.ly/2nmipMR';
-    }
-    
+function NewTab() { 
+            window.open( 
+              "https://bit.ly/2nmipMR","_self"); 
+        } 
