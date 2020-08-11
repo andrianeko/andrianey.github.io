e0a84537039166cb330d61f50ee167ab90bb4eaf
Andrian Eko <andrianeko.037@gmail.com>
2020-08-11 16:35:52 +0700

    Redo

---
+++ src/js/script.js
@@ -0,0 +1,75 @@
+$(window).load(function() {
+    $('.preloader').fadeOut('slow');
+ });
+ window.addEventListener('load' , function(){
+   function start(){
+   document.getElementById('Top').style.animation = "entrance 0.8s ease-out";
+   }
+ });
+
+
+
+ function NewTab() { 
+             window.open( 
+               "https://bit.ly/2nmipMR","_blank"); 
+         } 
+ 
+ function Instagram() { 
+             window.open( 
+               "https://instagram.com/andrian.ey","_blank"); 
+         } 
+ 
+ function Dribbble() { 
+             window.open( 
+               "https://dribbble.com/andrianey","_blank"); 
+         } 
+ 
+ function Behance() { 
+             window.open( 
+               "https://behance.net/andrianey","_blank"); 
+         } 
+ 
+ function Email() { 
+             window.open( 
+               "mailto:andrianeko.037@gmail.com","_blank"); 
+         } 
+ 
+ function Telegram() { 
+             window.open( 
+               "https://t.me/andrianey","_blank"); 
+         } 
+ 
+ 
+ function LinkedIn() { 
+             window.open( 
+               "https://Linkedin.com/in/andrianey","_blank"); 
+         } 
+ function Issuu() { 
+            window.open( 
+              "https://issuu.com/andrianey","_blank"); 
+        } 
+
+        $( ".card" ).mousedown(function() {
+          $( this ).addClass("active");
+          $( ".showtext-wrapper" ).addClass("active");
+          $( ".showimage-wrapper" ).addClass("active");
+        });
+        $( ".card" ).mouseleave(function() {
+          $( this ).removeClass("active");
+          $( ".showtext-wrapper" ).removeClass("active");
+          $( ".showimage-wrapper" ).removeClass("active");
+        });
+         gsap
+        .timeline({
+          repeat: -1,
+          defaults: {
+            duration: 3
+          }
+        })
+        .to('.id-container', {
+          y: 50,
+        })
+        .to('.id-container', {
+          y:0,})
+        ;
+      
